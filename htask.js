ea1512fb027a57512173bd34fbb85f1f9c87e1cc
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-06 23:16:12 +0100

    feat: working fine

--- htask.js
+++ htask.js
@@ -2,8 +2,9 @@ const readline = require('readline');
 const Task = require('./task');
 const request = require('request');
 const {Left, Rigth} = require('./either');
+const I = x => x;
 
-const prop = key => o => key in o ? Task.of(Left(o[key]).x) : Task.of(Rigth('no data'));
+const prop = key => o => key in o ? Task.of(Left(o[key]).chain(I)) : Task.of(Rigth('no data').chain(I));
 
 const geUserById = id => new Task((reject, resolve)=> {
   request(
@@ -11,7 +12,7 @@ const geUserById = id => new Task((reject, resolve)=> {
     { json: true }, 
     (err, res) => (err) ? reject(err) : resolve(res))
 });
-const ioReadLine = question => new Task((reject, resolve)=> {
+const obtainName = question => new Task((_, resolve)=> {
   const rl = readline.createInterface({ input: process.stdin, output: process.stdout});
   rl.question(question, (data) => {
     rl.close();
@@ -19,9 +20,10 @@ const ioReadLine = question => new Task((reject, resolve)=> {
   });
 });
 const LiftM2 = (f, a, b)=> {} 
-ioReadLine('preguntame el ID: ')
+
+obtainName('preguntame el ID: ')
   .chain(geUserById)
   .chain(prop('body'))
+  .chain(prop('company'))
   .chain(prop('name'))
-  
   .fork(console.log, console.log);
