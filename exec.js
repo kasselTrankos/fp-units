57f2b9ab5820d76edc81349bf8105c25d6c9295d
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-07 08:48:48 +0100

    feat: add liftM2

--- exec.js
+++ exec.js
@@ -10,9 +10,22 @@ io
   .ap(Monad(next => next(toUpperCase)))
   .chain(s => Monad(next => next (s.replace(/\n/, ''))))
   .map(s => `${s} no hay nueva linea`)
-  .map(s => s.split(' '))
+  .ap(Monad(next => next(s => s.split(' '))))
   .next(console.log);
 
+
+  // f a ~> f (a -> b) -> f b
+  const one = Monad.of([1,3,34,5])
+  const two = Monad.of([12])
+  const f = Monad.of()
+  const add = x => y => x + y;
+  one.ap(add).ap(two);
+
+  const liftA2 = (f, a, b) => b.ap(a.map(f))
+
+  
+  liftA2(x => y => x.map(v => y.map(g => v +g)), two, one)
+  .next(console.log)
 // standard_input.on('data', function (data) {
 
 //   // User input exit.
