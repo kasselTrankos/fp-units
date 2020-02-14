660e7107854d661863df330018796638912cd008
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-14 21:19:47 +0100

    feat: run trun

--- contexts.js
+++ contexts.js
@@ -43,7 +43,7 @@ const program = pipe (
 );
 
 
-program([1,2,3]).subscribe({
+program([1,2,3, 4,5,6]).subscribe({
   next: a => console.log('es muy mierdos', a),//console.log('next--->', a),
   complete: () =>  console.log('complete'),
   error: e => console.log(e, '<----errror')
