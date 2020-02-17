38bff7abbf5b7fb6a96d666770964957c834e38d
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-17 22:58:36 +0100

    feat: add more logic outside, this is bad and good becose it works alone from monad, but it must be solved by monad :)

--- contexts.js
+++ contexts.js
@@ -2,7 +2,7 @@ const {Stream} = require('./fp/monad');
 const {liftM, map, pipe, chain, ap, curry} = require('./fp/utils');
 const request = require('request');
 
-const append = x => xs => console.log('5555', x, new Date()) || [... xs, x];
+const append = x => xs => [... xs, x];
 const concat = x => xs => xs.concat(x);
 const flatmap = f => xs => xs.flatmap(f);
 const getByID= id => new Stream(({next, complete, error})=> {
@@ -16,7 +16,7 @@ const timer = d => new Stream(({next, complete, error})=> {
   const {author, delay} = d
   
   
-  setTimeout(()=> console.log(new Date(), 'next') || author=='Ra14mon' ? complete(author) : next(author+'next'+c), delay);
+  setTimeout(()=> next(author), delay);
 });
 
 
@@ -30,18 +30,27 @@ const insideOut = (T, xs) => xs.reduce((acc, x) => liftM(append, x, acc), T.of([
 const paralleliseTaskArray = f => data => insideOut(Stream, data.map(f))
 
 /// object :: f => Stream next
-
+const counter = ()=>{
+  let s =0;
+  return c => new Stream(({complete, next})=> {
+    s++;
+    next(c)
+    if(s===3) complete();
+  })
+};
 
 const program = pipe (
   paralleliseTaskArray(getByID),
   // chain(paralleliseTaskArray(timer)),
   flatmap(x=> x),
-  chain(timer)
+  chain(timer),
+  chain(counter())
+
 );
 
 
 program([1, 2, 3]).subscribe({
-  next: a => console.log('next: ', a, new Date()),//console.log('next--->', a),
+  next: a => console.log('send new iriigate next: ', a, new Date()),//console.log('next--->', a),
   complete: () =>  console.log('complete', new Date()),
   error: e => console.log(e, '< get - tjs---errror')
 });
