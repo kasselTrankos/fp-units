305bd36cc4afbedc469a1c61fbff61143e369f95
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-18 20:45:09 +0100

    feat: using a join is more clearer

--- contexts.js
+++ contexts.js
@@ -1,56 +1,60 @@
 const {Stream} = require('./fp/monad');
-const {liftM, map, pipe, chain, ap, curry} = require('./fp/utils');
+// const {liftM, map, pipe, chain, ap, curry} = require('./fp/utils');
 const request = require('request');
+const {chain, pipe} = require('ramda');
 
-const append = x => xs => [... xs, x];
-const concat = x => xs => xs.concat(x);
-const flatmap = f => xs => xs.flatmap(f);
+// const append = x => xs => [... xs, x];
+// const concat = x => xs => xs.concat(x);
+// const flatmap = f => xs => xs.flatmap(f);
 const getByID= id => new Stream(({next, complete, error})=> {
-  request(
-    `http://localhost:3000/posts/${id}`, 
+  return request(`http://localhost:3000/posts/${id}`, 
     { json: true }, 
     (err, res, body) => err ? complete(err) : next(body))
 });
-let c = 0;
+// let c = 0;
 const timer = d => new Stream(({next, complete, error})=> {
   const {author, delay} = d
   
-  
-  setTimeout(()=> next(author), delay);
+  const t = setTimeout(()=> next('author'), 100);
+  return ()=> clearInterval(t); // un sub
 });
 
 
 
-// insideOut :: Applicative f
-//           => [f a] -> f [a]
-const insideOut = (T, xs) => xs.reduce((acc, x) => liftM(append, x, acc), T.of([]))
+// // insideOut :: Applicative f
+// //           => [f a] -> f [a]
+// const insideOut = (T, xs) => xs.reduce((acc, x) => liftM(append, x, acc), T.of([]))
 
-// paralleliseTaskArray
-//   :: [Int] -> Stream e [User]
-const paralleliseTaskArray = f => data => insideOut(Stream, data.map(f))
+// // paralleliseTaskArray
+// //   :: [Int] -> Stream e [User]
+// const paralleliseTaskArray = f => data => insideOut(Stream, data.map(f))
 
-/// object :: f => Stream next
-const counter = ()=>{
-  let s =0;
-  return c => new Stream(({complete, next})=> {
-    s++;
-    next(c)
-    if(s===3) complete();
-  })
-};
+// /// object :: f => Stream next
+// const counter = ()=>{
+//   let s =0;
+//   return c => new Stream(({complete, next})=> {
+//     s++;
+//     next(c)
+//     if(s===3) complete();
+//   })
+// };
 
-const program = pipe (
-  paralleliseTaskArray(getByID),
-  // chain(paralleliseTaskArray(timer)),
-  flatmap(x=> x),
-  chain(timer),
-  chain(counter())
+// const program = pipe (
+//   chain(timer)
+//   // paralleliseTaskArray(getByID),
+//   // chain(paralleliseTaskArray(timer)),
+//   // flatmap(x=> x),
+//   // chain(timer),
+//   // chain(counter())
 
-);
+// );
 
 
-program([1, 2, 3]).subscribe({
-  next: a => console.log('send new irigate next: ', a, new Date()),//console.log('next--->', a),
-  complete: () =>  console.log('complete', new Date()),
+Stream.of(1)
+.chain(getByID)
+.chain(timer)
+.subscribe({
+  next: a => console.log('NEXT: ', a, new Date()),//console.log('next--->', a),
+  complete: () =>  console.log('completeTTT', new Date()),
   error: e => console.log(e, '< get - tjs---errror')
 });
--- fp/monad/stream.js
+++ fp/monad/stream.js
@@ -1,10 +1,10 @@
 const noop = () => {};
-function Stream(constrctor) {
-  this._constructor = constrctor;
+function Stream(constructor) {
+  this._constructor = constructor;
 }
 
 function run ({next, complete, error}) {
-  this._constructor({
+  return this._constructor({
     next: next || noop,
     complete: complete || noop,
     error: error || noop
@@ -17,7 +17,10 @@ Stream.prototype.subscribe = function(o) {
 
 // of :: Aplicative f => f ~> a -> f a
 Stream.of = function(x) {
-  return new Stream(({next}) => next(x));
+  return new Stream(({next}) =>{
+    next(x);
+    return ()=> {} // unsubs
+  });
 }
 
 // ap :: Apply f => f a ~> f(a -> b) -> f b
@@ -42,18 +45,50 @@ Stream.prototype.flatmap = function(f) {
     
   }));
 }
-
 // chain :: Chain m => m a ~> ( a -> m b) -> m b
 Stream.prototype.chain = function(m) {
-  return new Stream(handler => run.call(this, {
-    next: x =>  m(x)._constructor({
-      next: handler.next, 
-      error: handler.error,
-      complete: handler.complete
-    }),
-    complete: () => handler.complete(), // void, by definition you no must do nothing at this point
-    error: e => handler.error(e)
-  }));
+  return this.map(m).join();
+}
+// join :: 
+Stream.prototype.join = function() {
+  let streams = 0;
+  let completes = 0;
+  let subs = [];
+  let _stream, __stream;
+
+  return new Stream(observer => {
+    _stream = this.subscribe({
+      next: stream => {
+        streams++;
+        console.log('coun', streams);
+        __stream = stream.subscribe({
+          next: value => {
+            console.log('12-------------3 complete never complete>:; ', completes, 'streams -->', streams)
+            observer.next(value);
+            if(streams === completes){
+              observer.complete();
+            }
+          },
+          complete: () => {
+            observer.complete();
+          },
+          error: observer.error
+        });
+        
+      },
+      complete: () => {
+        console.log(completes, 'calla tin');
+        completes++;
+      }, 
+      error: e => observer.error(e)
+    }
+  );
+  return ()=> {
+    _stream();
+    __stream();
+  }
+
+});
 }
 
 
