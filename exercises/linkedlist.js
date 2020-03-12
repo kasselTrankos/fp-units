f466301ca968bd44016ae6f518f81e2e22979838
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-12 16:03:18 +0100

    feat: - time

--- exercises/linkedlist.js
+++ exercises/linkedlist.js
@@ -1,30 +1,5 @@
 const {LinkedList} = require('./../fp/monad');
-const daggy = require('daggy');
-const {composeK, chain} = require('ramda');
-
-const {map, reduce, concat, pipe} = require('ramda');
-const a = [{a: 1}, {a: 2}, {a: 190}];
-const b = [{a: 1}, {a: 19}, {a: 100}];
-const t = LinkedList.from([1,234])
-const aL = LinkedList.from(a);
-const bL = LinkedList.from(b);
-
-
-const { Loop, Done } = daggy.taggedSum('chainRec', {
-  Done: ['b'], Loop: ['a']
-})
-
-
-//   console.log(ta, 'harding power', whereNext('LAX'))
-const logger = List  => {
-	if(!List) {
-		console.log('end-< ', List)
-	} else {
-		const [car, cdr] = List.Cons;
-		console.log(car, ' tiem car')
-		logger(cdr)
-	}
-}
+const {composeK, chain, compose} = require('ramda');
 
 const log = message => (acc, elm)=> console.log(message, elm)
 
@@ -42,8 +17,9 @@ const _c = x => {
 // (associativity)
 //m['fantasy-land/chain'](f)['fantasy-land/chain'](g) 
 // m.chain(f).chain(g)
-LinkedList.from([1, 2, 3]).chain(_ch).chain(_c).reduce(log('Associative chainging: '), undefined);
+LinkedList.from([1, 2, 3]).chain(_ch).chain(_c).reduce(log(`Associative chainging ${new Date().getTime()}: `), undefined);
+console.log('===================')
 // is equivalent to m['fantasy-land/chain'](x => f(x)['fantasy-land/chain'](g)) 
 // chain(composeK(f, g))(m)
-chain (composeK (_c, _ch)) (LinkedList.from([1, 2, 3])).reduce(log('Associative compose : '), undefined);
+chain (composeK (_c, _ch)) (LinkedList.from([1, 2, 3])).reduce(log(`Associative compose ${new Date().getTime()} : `), undefined);
 
