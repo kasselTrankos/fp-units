af1c5eccbe9440f68db7a327c715bb767f84abc5
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-12 15:55:10 +0100

    feat: - complicated exercism of recursivity

--- exercises/linkedlist.js
+++ exercises/linkedlist.js
@@ -1,4 +1,4 @@
-const {LinkedList} = require('./fp/monad');
+const {LinkedList} = require('./../fp/monad');
 const daggy = require('daggy');
 const {composeK, chain} = require('ramda');
 
@@ -26,23 +26,24 @@ const logger = List  => {
 	}
 }
 
-const log = (acc, elm)=> console.log('i cant belive is true: ', elm)
+const log = message => (acc, elm)=> console.log(message, elm)
 
 
 // chain
 const _ch = x => {
   const [car, cdr] = x;
-  return LinkedList.of(car + 90, cdr)
+  return LinkedList.of(90, LinkedList.of(car, cdr))
 }
 const _c = x => {
   const [car, cdr] = x;
-  return LinkedList.of(car, LinkedList.of(cdr.Cons[0] + 200))
+  const [head, tail] = cdr.Cons;
+  return LinkedList.of(car, LinkedList.of(head + 200, tail))
 }
 // (associativity)
 //m['fantasy-land/chain'](f)['fantasy-land/chain'](g) 
 // m.chain(f).chain(g)
-LinkedList.from([1, 2]).chain(_ch).chain(_c).reduce(log, undefined);
+LinkedList.from([1, 2, 3]).chain(_ch).chain(_c).reduce(log('Associative chainging: '), undefined);
 // is equivalent to m['fantasy-land/chain'](x => f(x)['fantasy-land/chain'](g)) 
 // chain(composeK(f, g))(m)
-chain (composeK (_ch, _c)) (LinkedList.from([1, 2])).reduce(log, undefined);
+chain (composeK (_c, _ch)) (LinkedList.from([1, 2, 3])).reduce(log('Associative compose : '), undefined);
 
