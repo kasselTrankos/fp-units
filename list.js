f202f01f6e6509578d5f9cb2065d37fd6cb6d9f0
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-19 22:26:07 +0100

    feat:  - begin to learn concepts of use

--- list.js
+++ list.js
@@ -1,3 +1,5 @@
 const {LinkedList} = require('./fp/monad');
 const list = new LinkedList(1, [1, [2]]);
-console.log(list, LinkedList.of(1).reduce((acc, x)=> [...acc, x + 9000], []));
+const b = LinkedList.of(189).reduce((acc, x)=> [...acc, x + 9000], []);
+const c = LinkedList.of(90).reduce((acc, x) => acc.concat(LinkedList.of(x)), LinkedList.of(12))
+console.log(list, b, c);
