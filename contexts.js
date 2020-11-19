6e483b1b5e2d5546b1c9e6650664446efa5b4fcd
kasselTrankos <alvaro.touzon@gmail.com>
2020-11-20 00:11:29 +0100

    dont loose this

--- contexts.js
+++ contexts.js
@@ -20,7 +20,7 @@ const getByID= id => new Stream(({next, complete, error})=> {
 const timer = d => new Stream(({next, complete, error})=> {
   const delay = 900;
   const t = setTimeout(()=> {
-    if(d >1300) complete();
+    if(d >2300) complete();
     return next(d)
   }, delay + d);
   return ()=> clearInterval(t); // un sub
@@ -33,10 +33,11 @@ const program = pipe(
   chain(timer)
 );
 
-const _unbsus = program([1000, 1100, 1400, 1700])
+const _unbsus = program([1000, 1100, 1400, 1700, 2100, 2200, 2490])
 .subscribe({
   next: a => console.log('NEXT: ', a, new Date()),//console.log('next--->', a),
   complete: () =>  console.log('completeTTT', new Date()),
   error: e => console.log(e, '< get - tjs---errror'),
 });
+setTimeout(()=>_unbsus(), 2600);
 // console.log(_unbsus())
